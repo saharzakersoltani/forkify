1c03875a26f282ba568fde2d690e8e2f7846ec94
ManyToMany <sahar.zs2002@gmail.com>
2025-08-06 18:24:21 +0330

    loading a recipe from api

--- src/js/controller.js
+++ src/js/controller.js
@@ -12,3 +12,32 @@ const timeout = function (s) {
 // https://forkify-api.jonas.io
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.jonas.io/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    if (!res.ok) throw new Error(`some thing happened! (${res.status})`);
+
+    const data = await res.json();
+    console.log(res, data);
+
+    let { recipe } = data.data;
+    recipe = {
+      cookingTime: recipe.cooking_time,
+      id: recipe.id,
+      image: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      title: recipe.title,
+    };
+    console.log(recipe);
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+showRecipe();
